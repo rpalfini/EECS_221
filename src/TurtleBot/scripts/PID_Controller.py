17d6ba03a2fa34f28e8b539e25bdc72a68cf27e1
Robert Palfini <robert.palfini@gmail.com>
2023-05-09 12:32:40 -0700

    removing code for inputting location via keyboard

--- src/TurtleBot/scripts/PID_Controller.py
+++ src/TurtleBot/scripts/PID_Controller.py
@@ -2,27 +2,30 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from gazebo_msgs.msg import ModelStates
 from turtlesim.msg import Pose
-from TurtleBot.msg import Reference_Pose
+from TurtleBot.msg import Reference_Pose, PID_Gains
 import swim_to_goal as util
-import click
 
 class SubscriberNode(object):
     def __init__(self,topic,msg):
         self.data = None
         self.prev_data = None
-        # self.last_callback_time = 0
-        # self.time_since_last_callback = None
         rospy.Subscriber(topic, msg, self.callback)
-        # self.not_first_callback = False
 
     def callback(self, data):
         self.prev_data = self.data
         self.data = data
-        # callback_time = rospy.get_time()
-        # if self.not_first_callback:
-        #     self.time_since_last_callback = callback_time - self.last_callback_time
-        # self.last_callback_time = callback_time
+
+class SubscriberNodeUpdateGains(SubscriberNode):
+    def __init__(self, topic, msg, gains_obj):
+        super().__init__(topic, msg)
+        self.gains_obj = gains_obj
+
+    def callback(self, data):
+        super().callback(data)
+        self.gains_obj.update_gains(data)
+    
 
 class fakeSubscriberNode(object):
     '''used to match the interface needed for activating the controller'''
@@ -33,16 +36,17 @@ class fakeSubscriberNode(object):
         msg.theta = ref_theta
         msg.mode = mode
         self.data = msg
+
 class PID_gains(object):
     def __init__(self,Kp,Ki,Kd):
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
 
-    def update_gains(self,Kp,Ki,Kd):
-        self.Kp = Kp
-        self.Ki = Ki
-        self.Kd = Kd
+    def update_gains(self,gains_msg):
+        self.Kp = gains_msg.Kp
+        self.Ki = gains_msg.Ki
+        self.Kd = gains_msg.Kd
 
 class err_struct(object):
     def __init__(self):
@@ -53,7 +57,7 @@ class err_struct(object):
         self.prev_int_err = 0
         self.error_max = 100
     
-    def record_err(self,new_err,dt):
+    def record_err(self,new_err):
         # dt is the time between measurements
         self.update_err(new_err)
         new_int_err = self.accumulate_error(new_err)
@@ -88,15 +92,8 @@ class err_struct(object):
     def update_d_error(self,new_err):
         self.deriv_error = (new_err - self.prev_err)
 
-def get_gains_from_topic():
-    #TODO implement getting PID gain values from topic
-    return None
-
 def pid(gains,err_struct):
     return gains.Kp*err_struct.err + gains.Ki*err_struct.int_err + gains.Kd*err_struct.d_err
-
-def format_target(node):
-    return (node.data.pose.x, node.data.pose.y)
 
 def is_final_angle(pose,ref_theta):
     # checks if body is at final reference angle
@@ -107,39 +104,21 @@ def is_final_angle(pose,ref_theta):
         result = False
     return result
 
-def debug_interval(dbg_state,iterations,interval):
-    if dbg_state and iterations % interval == 0:
-        return True
-    else:
-        return False
-
-def debug_info(info,**kwargs):
-    output_string = info + " "
-    for key,value in kwargs.iteritems():
-        output_string =+ str(key) + ": " + str(value) + ','
-    rospy.loginfo(output_string[:-2])
-
-def request_mode():
-    return util.request_number('')
-
-def request_ref_point():
-    x = util.request_number('x',is_bounds=False)
-    y = util.request_number('y',is_bounds=False)
-    theta = util.request_number('theta (radians)',is_bounds=False)
-    return (x,y,theta)
-
 def main():
     rospy.init_node('PID_Controller')
+    # test_mode specifies if 
+    test_mode = True
     # setup subscriptions
     pos_node = SubscriberNode(topic='/turtle1/pose',msg = Pose)
     ref_node = SubscriberNode(topic='/reference_pose',msg = Reference_Pose)
-    mp_node = SubscriberNode(topic='/')
+    pos_gains_node = SubscriberNode(topic='/pos_gains',msg = PID_Gains)
+    ang_gains_node = SubscriberNode(topic='/ang_gains',msg = PID_Gains)
     # setup publisher
-    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
     r = rospy.Rate(10)
     dbg = True
     debug_interval = 10
-    test_mode = True
+    
    
     pos_gains = PID_gains(Kp=1.5,Ki=0,Kd=0)
     ang_gains = PID_gains(Kp=3,Ki=0,Kd=0)
@@ -148,35 +127,37 @@ def main():
     
     iterations = 0 # used for debugging
 
-    if test_mode:
-        # in this mode gains and references are comanded my motion planner node
-        activate_controller(pos_node, ref_node, pub, r, dbg, debug_interval, pos_gains, ang_gains, pos_err, ang_err, iterations)
-    else:
-        # gains are hard coded and references are inputted by user
-        mode = request_mode()
-        ref_value = request_ref_point()
-        fake_ref_node = fakeSubscriberNode(*ref_value,mode)
-        activate_controller(pos_node, fake_ref_node, pub, r, dbg, debug_interval, pos_gains, ang_gains, pos_err, ang_err, iterations)
+    while not rospy.is_shutdown():
+        if test_mode:
+            # in this mode gains are received via topic
+            activate_controller(pos_node, ref_node, pub, r, dbg, debug_interval, pos_gains, ang_gains, pos_err, ang_err, iterations)
+        else:
+            # gains are hard coded and references are inputted by user
+            mode = request_mode()
+            ref_value = request_ref_point()
+            fake_ref_node = fakeSubscriberNode(*ref_value,mode)
+            activate_controller(pos_node, fake_ref_node, pub, r, dbg, debug_interval, pos_gains, ang_gains, pos_err, ang_err, iterations)
 
-
-def activate_controller(pos_node, ref_node, pub, r, dbg, debug_interval, pos_gains, ang_gains, pos_err, ang_err, iterations):
+## Controller Functions
+def activate_controller(pos_node, ref_node, pub, r, dbg, debug_interval, pos_gains, ang_gains, pos_err, ang_err, iterations, test_mode):
     is_final_pose = False
-    while not is_final_pose() and not rospy.is_shutdown():
+    while not is_final_pose and not rospy.is_shutdown():
         is_arrived = util.check_if_arrived(pos_node.data,format_target(ref_node))
-        if not is_arrived:
-            if ref_node.data.mode == 0:
-                # first turn to face target, then move to target, then adjust to final reference angle
-                rospy.loginfo('Using Mode 0')
-                turn_to_target(pos_node, ref_node, pub, r, dbg, debug_interval, ang_gains, ang_err, iterations)
-                move_to_target(pos_node, ref_node, pub, r, dbg, debug_interval, pos_gains, pos_err, iterations)
-                turn_to_ref_theta(pos_node, ref_node, pub, r, dbg, debug_interval, ang_gains, ang_err, iterations)
-                is_final_pose = True
+        if test_mode: 
+            if not is_arrived:
+                if ref_node.data.mode == 0:
+                    # first turn to face target, then move to target, then adjust to final reference angle
+                    rospy.loginfo('Using Mode 0')
+                    turn_to_target(pos_node, ref_node, pub, r, dbg, debug_interval, ang_gains, ang_err, iterations)
+                    move_to_target(pos_node, ref_node, pub, r, dbg, debug_interval, pos_gains, pos_err, iterations)
+                    turn_to_ref_theta(pos_node, ref_node, pub, r, dbg, debug_interval, ang_gains, ang_err, iterations)
+                    is_final_pose = True
 
-            elif ref_node.data.mode == 1:
-                # turn and move to target at the same time
-                rospy.loginfo('Using Mode 1')
-                move_and_turn_to_target(pos_node, ref_node, pub, r, dbg, debug_interval, ang_gains, ang_err, pos_gains, pos_err, iterations)
-                is_final_pose = True
+                elif ref_node.data.mode == 1:
+                    # turn and move to target at the same time
+                    rospy.loginfo('Using Mode 1')
+                    move_and_turn_to_target(pos_node, ref_node, pub, r, dbg, debug_interval, ang_gains, ang_err, pos_gains, pos_err, iterations)
+                    is_final_pose = True
 
 def move_and_turn_to_target(pos_node, ref_node, pub, r, dbg, debug_interval, ang_gains, ang_err, pos_gains, pos_err, iterations):
         # move and turn to target
@@ -244,6 +225,30 @@ def move_to_target(pos_node, ref_node, pub, r, dbg, debug_interval, pos_gains, p
         iterations += 1
         r.sleep()
 
+## Utility Functions
+def format_target(node):
+    return (node.data.pose.x, node.data.pose.y)
+
+def request_mode():
+    return util.request_number('')
+
+def request_ref_point():
+    x = util.request_number('x',is_bounds=False)
+    y = util.request_number('y',is_bounds=False)
+    theta = util.request_number('theta (radians)',is_bounds=False)
+    return (x,y,theta)
+
+def debug_interval(dbg_state,iterations,interval):
+    if dbg_state and iterations % interval == 0:
+        return True
+    else:
+        return False
+
+def debug_info(info,**kwargs):
+    output_string = info + " "
+    for key,value in kwargs.iteritems():
+        output_string =+ str(key) + ": " + str(value) + ','
+    rospy.loginfo(output_string[:-2])
 
 if __name__ == "__main__":
     main()
