4395638b02158df8b37f7018b16cc6f669f450aa
Robert <rpalfini@uci.edu>
2023-05-17 23:47:13 -0700

    fixed rrt with my bfs implementation

--- src/TurtleBot/scripts/rrt.py
+++ src/TurtleBot/scripts/rrt.py
@@ -41,6 +41,52 @@ class SubscriberNode_Map(pid.SubscriberNode):
       current_col += 1  
     return map_out 
 
+class node_obj(object):
+    def __init__(self,node,parent):
+        self.state = node #this should be (x,y) value
+        self.parent = parent #this should refer to the parent node_obj
+
+def is_present(target, lst):
+    return target in lst
+
+def BFS(graph,start):
+    frontier = []
+    visited = []
+    # find initial edge
+    for node_edge in graph:
+        if start == node_edge[0]:
+            node2expand = node_edge
+            break
+    # add first level deep to initialize frontier
+    start_node = node_obj(node2expand[0],None)
+    visited.append(node2expand[0])
+
+    for child in node2expand[1]:
+        frontier.append(node_obj(child,start_node))
+    
+
+    while not len(frontier) == 0:
+        node2explore = frontier.pop(0)
+        visited.append(node2explore.state)
+        if node2explore.state == graph[-1][0]: #check if node_edge is goal
+            break
+        for node_edge in graph:
+            if node2explore.state == node_edge[0]:
+                node2expand = node_edge
+                break
+        for child in node2expand[1]:
+            if not is_present(child,visited):
+                frontier.append(node_obj(child,node2explore))
+
+    path = [node2explore.state]
+    parent_node = node2explore.parent
+    # while not parent_node.state == start:
+    while not parent_node is None:
+        node2explore = parent_node
+        parent_node = node2explore.parent
+        path.insert(0,node2explore.state)
+    return path  
+
 def rapidlyExploringRandomTree(img, start, goal, seed=None):
   rospy.loginfo('entered rapidlyExploringRandomTree')
   hundreds = 100
