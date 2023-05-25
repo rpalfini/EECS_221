f4a60fba3230cca9e03b6a466a0649d54df15417
Robert Palfini <robert.palfini@gmail.com>
2023-05-24 18:48:05 -0700

    found out how to read pickle data between python2 and python3

--- src/TurtleBot/tests/test_live_map_read.py
+++ src/TurtleBot/tests/test_live_map_read.py
@@ -28,16 +28,24 @@ def plot_map(map_data):
 
 
 def main():
+    plt.figure()
+    plt.ion()
     rospy.init_node('Map_Explorer_Node')
     map_node = rrt.SubscriberNode_Map('/map',OccupancyGrid,OccupancyGrid())
-    with open('map_node.pickle','wb') as f:
-        f.dump({'map': map_node.current_map},f)
-    plot_map(map_node.current_map)
+    while not map_node.is_received() and not rospy.is_shutdown():
+        rospy.sleep(0.5)
+    with open('map_node.pkl','wb') as f:
+        output = {'map': map_node.current_map}
+        # output = {'map': 5}
 
 
-
-
-
+        print(type(map_node.current_map))
+        pickle.dump(output,f,protocol=2)
+    while not rospy.is_shutdown():
+        plot_map(map_node.current_map)
+        rospy.loginfo('map updated')
+        rospy.sleep(2)
+    
 
 
 if __name__ == "__main__":
