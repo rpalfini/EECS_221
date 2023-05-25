65ef17181208c46850c9b3a91e6ee19cc993d9ef
Robert Palfini <robert.palfini@gmail.com>
2023-05-24 19:38:10 -0700

    got active plotting working for reading map updates

--- src/TurtleBot/tests/test_live_map_read.py
+++ src/TurtleBot/tests/test_live_map_read.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from scipy.misc import imread
 import random, sys, math, os.path
 import cv2
+import pickle
 
 from nav_msgs.msg import OccupancyGrid
 from std_msgs.msg import Float64MultiArray
@@ -18,13 +19,21 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../scripts")
 import PID_Controller as pid
 import Motion_Planner as mp
 import rrt
-import pickle
+
+
+def adjust_map_colors(map_in):
+    # replaces the map colors with easier to see values
+    # map_out = np.where(map_in == 100,0,map_in)
+    map_out = np.where(map_in == 0, 100, np.where(map_in == 100, 0, np.where(map_in == -1, 50, map_in)))
+    return map_out
 
 
 def plot_map(map_data):
-    # expects mapp as np array
+    # expects map_data as np array
+    # map_data = np.where(map_data == -1,50,map_data) #distinguishes 
+    map_data = adjust_map_colors(map_data)
     plt.imshow(map_data,cmap='gray')
-    plt.show()
+    # plt.show()
 
 
 def main():
@@ -34,17 +43,23 @@ def main():
     map_node = rrt.SubscriberNode_Map('/map',OccupancyGrid,OccupancyGrid())
     while not map_node.is_received() and not rospy.is_shutdown():
         rospy.sleep(0.5)
-    with open('map_node.pkl','wb') as f:
-        output = {'map': map_node.current_map}
-        # output = {'map': 5}
+    # with open('map_node.pkl','wb') as f:
+    #     output = {'map': map_node.current_map}
+    #     # output = {'map': 5}
 
-
-        print(type(map_node.current_map))
-        pickle.dump(output,f,protocol=2)
+    #     print(type(map_node.current_map))
+    #     pickle.dump(output,f,protocol=2)
+    
     while not rospy.is_shutdown():
+        # if np.array_equal(map_node.prev_map,map_node.current_map):
+        #     rospy.loginfo('map is the same')
+        # else:
+        #     rospy.loginfo('map is different')
         plot_map(map_node.current_map)
+        
         rospy.loginfo('map updated')
-        rospy.sleep(2)
+        plt.pause(0.5)
+        plt.show()
     
 
 
